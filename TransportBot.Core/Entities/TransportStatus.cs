d5209befe11d6c3ee1031df8093fcdd81e1422c8
Nick <kved0610@mail.ru>
2025-08-31 19:39:11 +0400

    Issue #1: Implemented Core Domain Entities

---
+++ TransportBot.Core/Entities/TransportStatus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportBot.Core.Entities
+{
+    public class TransportStatus
+    {
+        public int Id { get; set; }
+
+        public int RouteId { get; set; }
+        public Route Route { get; set; }
+
+        public int StopId { get; set; }
+        public TransportStop Stop { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
